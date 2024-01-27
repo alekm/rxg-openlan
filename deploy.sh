f6f015ac1151d36f5cee8d6e12999ab21070d70c
alekm <alekmurray@gmail.com>
2024-01-27 04:34:55 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,21 +19,57 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 
 # clone openwifi controller to /opt/openwifi
 sudo git clone https://github.com/Telecominfraproject/wlan-cloud-ucentral-deploy.git /opt/openwifi
-sudo docker-compose --project-directory /opt/openwifi/docker-compose pull
 
 # edit default user/pass
 printf "\n\nEnter initial email login:\n"
 read loginemail
 printf "\n"
-printf "Enter initial password:\n"
+printf "Enter initial web interface password (required to change at first login):\n"
 read initialpass
 userpass="${initialpass}${loginemail}"
 passhash=$(echo -n "${userpass}"|shasum -a 256 | cut -d" " -f1)
 
-
 echo "AUTHENTICATION_DEFAULT_USERNAME=${loginemail}" | sudo tee -a /opt/openwifi/docker-compose/owsec.env > /dev/null
 echo "AUTHENTICATION_DEFAULT_PASSWORD=${passhash}" | sudo tee -a /opt/openwifi/docker-compose/owsec.env > /dev/null
 
+# setup ENV variables for public deployment
+
+printf "\n\nEnter FQDN for public accessibility (Blank for private self-signed only, domain must exist):\n"
+
+read fqdn 
+
+# if FQDN exists, set env variables for LetsEncrypt cert pull
+
+if [ -n "$fqdn" ]; then
+	printf "\n\nEnter email for LetsEncrypt certificate:"
+	read leemail
+	
+cat <<EOF |  sudo tee -a /etc/environment > /dev/null
+SDKHOSTNAME=$fqdn
+DEFAULT_UCENTRALSEC_URL=https://$fqdn:16001
+SYSTEM_URI_UI=https://$fqdn:16001
+OWGW_FILEUPLOADER_HOST_NAME=$fqdn
+OWGW_FILEUPLOADER_URI=https://$fqdn:16003
+OWGW_SYSTEM_URI_PUBLIC=https://$fqdn:16002
+OWGW_RTTY_SERVER=$fqdn
+OWSEC_SYSTEM_URI_PUBLIC=https://$fqdn:16001
+OWFMS_SYSTEM_URI_PUBLIC=https://$fqdn:16004
+OWPROV_SYSTEM_URI_PUBLIC=https://$fqdn:16005
+OWANALYTICS_SYSTEM_URI_PUBLIC=https://$fqdn:16009
+OWSUB_SYSTEM_URI_PUBLIC=https://$fqdn:16006
+OWRRM_SERVICECONFIG_PRIVATEENDPOINT=https://openwifi.wlan.local:16789
+OWRRM_SERVICECONFIG_PUBLICENDPOINT=https://$fqdn:16789
+TRAEFIK_TAG=latest
+INTERNAL_OWGW_HOSTNAME=openwifi.wlan.local
+INTERNAL_OWPROVUI_HOSTNAME=openwifi.wlan.local
+OWRRM_TAG=main
+INTERNAL_OWRRM_HOSTNAME=openwifi.wlan.local
+TRAEFIK_CERTIFICATESRESOLVERS_OPENWIFI_ACME_EMAIL=${leemail}
+EOF
+fi	
+
+
 
 # deploy controller
-sudo docker-compose --project-directory /opt/openwifi/docker-compose up -d
+cd /opt/openwifi/docker-compose/
+sudo ./deploy.sh
